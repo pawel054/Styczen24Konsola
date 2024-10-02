871ec3f5432f5be8daf91f0c3ebfaa04ddfabfc2
pawel054 <pawel.marcisz1@gmail.com>
2024-10-02 10:41:34 +0200

    Dodaj pliki projektów.

---
+++ AplikacjaKonsola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AplikacjaKonsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
