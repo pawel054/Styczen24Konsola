d8495fac5e2fb0848d948464b01bfe782862f4d0
pawel054 <pawel.marcisz1@gmail.com>
2024-10-09 08:25:00 +0200

    Add CheckSum function

--- AplikacjaKonsola/Program.cs
+++ AplikacjaKonsola/Program.cs
@@ -15,11 +15,19 @@
             {
                 Console.WriteLine("Mężczyzna");
             }
+            if (CheckSum(pesel))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
 
         private static char CheckGender(char[] pesel)
         {
-            if (pesel[9]%2 == 0)
+            if ((pesel[9] - '0') % 2 == 0)
             {
                 return 'K';
             }
@@ -27,6 +35,29 @@
             {
                 return 'M';
             }
+        }
+
+        private static bool CheckSum(char[] pesel)
+        {
+            int[] weigths = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int S = 0;
+            int R = 0;
+
+            for (int i = 0; i<10; i++)
+            {
+                S += (pesel[i] - '0') * weigths[i];
+            }
+
+            int M = S % 10;
+            if(M == 0)
+                R = 0;
+            else
+                R = 10 - M;
+
+            if((pesel[10]-'0') == R)
+                return true;
+            else
+                return false;
         }
     }
 }
